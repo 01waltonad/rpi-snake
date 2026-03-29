fe3b317c306b384534c77ed907bff78266017b67
Andrew <andrewwalton1@gmail.com>
2026-03-29 21:46:47 +0100

    Typo fix
    Updated package management commands to use 'apt-get' instead of 'apt'.

--- Install.sh
+++ Install.sh
@@ -5,9 +5,9 @@
 # sets up 
 
 #updates rpi
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
 sudo localedef -i en_US -f UTF-8 en_US.UTF-8 # sorts out location issue. not sure why this appearing on new install
 sudo localedef -i en_GB -f UTF-8 en_GB.UTF-8 # sorts out location issue. not sure why this appearing on new install
 echo
@@ -18,7 +18,7 @@ echo Installing Influxdb...
 echo 
 echo
 sleep 2
-# gets key data and addes new repository
+# gets key data and adds new repository
 wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
 echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
